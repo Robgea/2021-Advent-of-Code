e67df05b6e60e765c72f825229d7bb88f7a03989
Robert Glass <git@github.com:Robgea/2021-Advent-of-Code.git>
2021-12-03 01:11:33 -0600

    Day One

---
+++ Day One/README.md
@@ -0,0 +1,5 @@
+**First**
+
+First one is off by one, not sure why. 
+
+
---
+++ Day One/first.py
@@ -0,0 +1,36 @@
+import csv
+
+def num_getter():
+	with open("nums.csv", encoding='utf-8') as raw_csv:
+		csv_read = csv.reader(raw_csv)
+		csv_list = list(csv_read)
+		csv_list.pop(0)
+		return_list = [int(row[0]) for row in csv_list]
+	return return_list
+
+
+def looper(input_list):
+	first_num = 'Start'
+	increase_count = 0
+	decrease_count = 0
+
+	for num in input_list:
+		if first_num == 'Start':
+			pass
+		else:
+			if num > first_num:
+				increase_count += 1
+
+		first_num = num
+
+	return increase_count
+
+
+def main():
+	nums = num_getter()
+	count = looper(nums)
+	print(count)
+
+
+if __name__ == '__main__':
+	main()
